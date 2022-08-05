5572b272115f54d97bb90ad6d2f2e262954f31df
Thomas Kaar <thomas.kaar@verizonmedia.com>
2022-08-05 12:56:28 -0700

    send voter info to backend

--- src/middlewares/castVote.js
+++ src/middlewares/castVote.js
@@ -1,13 +1,25 @@
 const axios = require('axios').default;
 const castVote = async (vote, pollId, userId, done) => {
     // TODO: Add in auth before this request
-    axios.post('http:localhost:8080/cast', {
-        pollId,
-        userId,
-        vote
+    axios({
+        method: 'post',
+        url: '/cast',
+        baseURL: 'http://localhost:8080',
+        headers: {
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "GET, POST, PATCH, PUT, DELETE, OPTIONS",
+            "Access-Control-Allow-Headers": "Origin, Content-Type, X-Auth-Token, Authorization, Accept,charset,boundary,Content-Length"
+        },
+        data: {
+            pollId,
+            userId,
+            vote
+        }
     }).then((response) => {
-        console.log(response);
-        done();
+        if (response.status === 200){
+            done();
+        }
     }).catch((err) => {
         console.log(err);
         done();
--- src/routes/vote.jsx
+++ src/routes/vote.jsx
@@ -13,25 +13,25 @@ export default function Vote({prompt, promptInfo}) {
         </p>
         <div>
             <button
-              class="block w-1/2 px-8 py-4 mx-auto mb-5 rounded-md shadow-md bg-green-600 hover:bg-green-800"
+              className="block w-1/2 px-8 py-4 mx-auto mb-5 rounded-md shadow-md bg-green-600 hover:bg-green-800"
               onClick={() => setApprove(true)}>
               yes
             </button>
-            <button class="block w-1/2 px-8 py-4 mx-auto mb-8 rounded-md shadow-md bg-red-600 hover:bg-red-800"
+            <button className="block w-1/2 px-8 py-4 mx-auto mb-8 rounded-md shadow-md bg-red-600 hover:bg-red-800"
             onClick={() => setApprove(false)}>
               no
             </button>
             <button
-              class="block w-1/2 px-8 py-4 mx-auto rounded-md bg-purple-600 hover:bg-purple-800 disabled:mt-16 disabled:bg-stone-600"
+              className="block w-1/2 px-8 py-4 mx-auto rounded-md bg-purple-600 hover:bg-purple-800 disabled:mt-16 disabled:bg-stone-600"
               disabled={approve !== true & approve !== false}
               onClick={() => {
-                  castVote(approve, prompt, 0, () => {window.location = '/'});
+                  castVote(approve, prompt, 0, () => setApprove(undefined));
               }}
               >
                 Cast Vote
             </button>
             {(approve === true || approve === false) && (
-              <span class="my-4">
+              <span className="my-4">
                   Confirm you are voting {approve ? 'yes' : 'no'} on {prompt}
               </span>
             )}
